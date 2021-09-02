a7fb908be5272ef710a4c3e11099cc38f0df0767
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-09-02 16:01:10 +0300

    Connected the context with components Button and Field with default value

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,15 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends React.Component {
+  /* Forms a link between the Class on Context */
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
     return (
       <div className='ui field'>
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,3 +1,8 @@
 import React from "react";
 
+/* We can use any type of JS object when passing
+   the createContext default value as string 
+   "englis". It can be also f.e. { color: "red" }
+   of array [1,2,3] 
+  */
 export default React.createContext("english");
