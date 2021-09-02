c36173cc5a7eec8fc02a9a9642d99f4be766e680
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-09-02 17:42:54 +0300

    Added a new LanguateStore component into the LanguageContext component

--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -2,7 +2,28 @@ import React from "react";
 
 /* We can use any type of JS object when passing
    the createContext default value as string 
-   "englis". It can be also f.e. { color: "red" }
+   "english". It can be also f.e. { color: "red" }
    of array [1,2,3] 
   */
-export default React.createContext("english");
+const Context = React.createContext("english");
+
+/* Named export, not a default */
+export class LanguageStore extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  /* React must have capital letter in name of component
+     it renders f.e. "Context" in this case. */
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
