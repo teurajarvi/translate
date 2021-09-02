30516ff3976ccfa75c5e3b9721ce7db9306b6faa
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-09-02 15:38:07 +0300

    Added a Context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,6 +1,15 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  /* contextType is a special property name!
+     "Button.contextType = LanguageContext" since 
+     the contextType is class Button property.
+     class can reference to the contextType by
+     this.context
+    */
+  static contextType = LanguageContext;
+
   render() {
     return <button className='ui button primary'>Submit</button>;
   }
